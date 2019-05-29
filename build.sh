978e2fcf34d8d1165608a1d23101f5d9f0c30cf0
Eric Allam <eallam@icloud.com>
2019-05-29 18:40:02 +0100

    Upgrade to 0.3.7

--- build.sh
+++ build.sh
@@ -20,16 +20,14 @@ elif [ "$1" = "deploy" ] && [ $# -eq 5 ]; then
   
   aws cloudformation package --template-file template.yml --output-template-file packaged.yml --s3-bucket $BUCKET_NAME --region $REGION
   aws cloudformation deploy --template-file packaged.yml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM --region $REGION --no-fail-on-empty-changeset
-elif [ "$1" = "publish" ] && [ $# -eq 5 ]; then
-  STAGE=$2
-  STACK_NAME=$3
-  REGION=$4
-  BUCKET_NAME=$5
-
-  echo $BUCKET_NAME
+elif [ "$1" = "publish" ] && [ $# -eq 2 ]; then
+  REGION=$2
+  ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" | tr -d \")
+  BUCKET_NAME="devops-${REGION}-${ACCOUNT_ID}.solvehq.com"
   
-  aws cloudformation package --template-file template.yaml --output-template-file packaged.yml --s3-bucket $BUCKET_NAME --region $REGION
-  sam publish --template-file packaged.yml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM --region $REGION
+  sam build
+  aws cloudformation package --template-file .aws-sam/build/template.yaml --output-template-file .aws-sam/build/packaged.yml --s3-bucket $BUCKET_NAME --region $REGION
+  sam publish --template .aws-sam/build/packaged.yml --region $REGION
 else
   echo $INSTRUCTION
   exit 1
