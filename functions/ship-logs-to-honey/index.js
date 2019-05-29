cf4732eed01e6152d9a314f291a4adb2fbb5a57d
Eric Allam <eallam@icloud.com>
2019-05-29 18:35:08 +0100

    Upgrade to libhoney-promise 1.0.1 to fix unresolved promise bug

--- functions/ship-logs-to-honey/index.js
+++ functions/ship-logs-to-honey/index.js
@@ -1,19 +1,24 @@
 const debug = require("debug")("ship-logs-to-honey");
-const { initHoneyClient, extractLogEvents, processAll } = require('./lib')
+const { initHoneyClient, extractLogEvents, processAll } = require("./lib");
 
-const { SECRET_ID } = process.env
+const { SECRET_ID } = process.env;
 
 let honeyClient;
 
 const handler = async event => {
-  debug(`received invocation event`, { event })
+  debug(`received invocation event`, { event });
 
   if (!honeyClient) {
     honeyClient = await initHoneyClient(SECRET_ID);
   }
 
+  if (!honeyClient) {
+    throw new Error(`Could not initialize the honeyClient with ${SECRET_ID}`);
+  }
+
   const cwLogEvents = extractLogEvents(event);
-  await processAll(cwLogEvents, honeyClient);
+
+  return processAll(cwLogEvents, honeyClient);
 };
 
 exports.handler = handler;
